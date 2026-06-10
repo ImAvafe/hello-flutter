14e9b38529207984fa0d86b07cfdcf40fabac9b1
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-06-10 14:55:59 +0800

    Add prereqs

--- wikipedia_reader/lib/main.dart
+++ wikipedia_reader/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart';
 import 'package:flutter/material.dart';
+import 'summary.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +13,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: AppBar(title: const Text('Wikipedia Reader')),
+        body: Center(child: Text('aaaaaaaa')),
       ),
     );
   }
