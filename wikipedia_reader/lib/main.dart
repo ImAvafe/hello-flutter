d5698b5a60a6823b262dbb3b1b3a6e1a0eee9177
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-06-10 15:39:29 +0800

    Implement article fetching view model

--- wikipedia_reader/lib/main.dart
+++ wikipedia_reader/lib/main.dart
@@ -13,11 +13,58 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = ArticleViewModel(ArticleModel());
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Wikipedia Reader')),
         body: Center(child: Text('aaaaaaaa')),
       ),
     );
+  }
+}
+
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw const HttpException('Failed to update resource!');
+    }
+
+    return Summary.fromJson(jsonDecode(response.body) as Map<String, Object?>);
+  }
+}
+
+class ArticleViewModel extends ChangeNotifier {
+  final ArticleModel model;
+  Summary? summary;
+  Exception? error;
+  bool isLoading = false;
+
+  ArticleViewModel(this.model) {
+    fetchArticle();
+  }
+
+  Future<void> fetchArticle() async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      summary = await model.getRandomArticleSummary();
+      error = null;
+      print('$summary!.titles.normalized');
+    } on HttpException catch (newError) {
+      error = newError;
+      summary = null;
+      print('$newError.message');
+    }
+
+    isLoading = false;
+    notifyListeners();
   }
 }
