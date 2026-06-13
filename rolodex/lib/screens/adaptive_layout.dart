c25683f7dda982509b8820439c0e983d1399dc2d
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-06-13 13:29:15 +0800

    Conditionally show sidebar on large screens

---
+++ rolodex/lib/screens/adaptive_layout.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'contact_groups.dart';
+
+const largeScreenMinWith = 600;
+
+class AdaptiveLayout extends StatefulWidget {
+  const AdaptiveLayout({super.key});
+
+  @override
+  State<AdaptiveLayout> createState() => _AdaptiveLayoutState();
+}
+
+class _AdaptiveLayoutState extends State<AdaptiveLayout> {
+  int selectedListId = 0;
+
+  void _onContactListSelected(int id) {
+    setState(() {
+      selectedListId = id;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final isLargeScreen = constraints.maxWidth >= largeScreenMinWith;
+
+        if (isLargeScreen) {
+          return _LargeScreenLayout();
+        } else {
+          return ContactGroupsPage();
+        }
+      },
+    );
+  }
+}
+
+class _LargeScreenLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.extraLightBackgroundGray,
+      child: SafeArea(
+        child: Row(
+          children: [
+            const SizedBox(width: 320, child: Text("sidebar")),
+            Container(width: 1, color: CupertinoColors.separator),
+            const Expanded(child: Text('details')),
+          ],
+        ),
+      ),
+    );
+  }
+}
